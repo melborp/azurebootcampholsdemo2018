2a3ae27580d42b584d0c1508f4742d4c9c053f48
Taavi Koosaar <taavik@solidify.se>
2018-04-18 23:31:03 +0200

    step 1 complete, but with obj

---
+++ WebAppAspNetCore/Models/RunnerPerformance.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WebAppAspNetCore.Models
+{
+    public class RunnerPerformance
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int FivekmTime { get; set; }
+    }
+
+}
